5f88762204fa37da8f50a7c970f640029f8d3e11
leanpv <lean.puebla@gmail.com>
2020-02-27 16:50:08 -0300

    hotfix

---
+++ frontend/src/app/components/panel/medics/medics.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit } from '@angular/core';
+import { MedicsService } from '../../../services/medics.service';
+import { Observable } from 'rxjs';
+import { Medics } from '../../../interfaces/medics';
+
+@Component({
+  selector: 'app-medics',
+  templateUrl: './medics.component.html',
+  styleUrls: ['./medics.component.css']
+})
+export class MedicsComponent implements OnInit {
+
+  data;
+  testData: Observable<Medics[]>;
+
+  constructor(private medicsServ: MedicsService) { }
+
+  ngOnInit(){
+    this.getData();
+    // this.testData = this.medicsServ.getMedics();
+  }
+
+  async getData() {
+    try {
+      this.data = await this.medicsServ
+      .getMedicos()
+      .toPromise();
+      this.data = this.data.consultorios;
+      console.log(this.data);
+    } catch (error) {}
+  }
+
+}
