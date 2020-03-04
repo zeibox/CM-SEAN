f3f2e5831c557bca033b24ed342b3951f8f88aed
Leandro <lean.puebla@gmail.com>
2020-03-04 04:28:10 -0300

    arreglos panel

--- backend/src/routes/keys.ts
+++ backend/src/routes/keys.ts
@@ -1,6 +1,6 @@
 export default {
     database: {
-        host: '68.183.27.141',
+        host: '68.183.128.14',
         user: 'zeiboxuser',
         password: 'CdLpJsMp775&+ñ',
         database: 'cm'
