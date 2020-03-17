88b6b73ff2fc9031038e221c3641aa1eaa94edf5
Javier <javiersoal@gmail.com>
2020-03-17 11:23:38 -0300

    domicilio sin funcionar

--- frontend/src/app/components/panel/domicilio/domicilio.component.ts
+++ frontend/src/app/components/panel/domicilio/domicilio.component.ts
@@ -1,15 +1,13 @@
 import { Component, OnInit} from '@angular/core';
 import { Validators, FormGroup, FormBuilder } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
-import { LocalidadesService } from '../../../services/localidades.service';
+import { DomiciliosService } from '../../../services/domicilios.service';
 import { DatePipe } from '@angular/common';
-import { Localidad } from '../../../interfaces/localidades';
-import { AreasService } from '../../../services/areas.service';
+import { Domicilio } from '../../../interfaces/domicilios';
 import { of } from 'rxjs';
 import { filter, tap } from 'rxjs/operators';
 import { PaisesService } from '../../../services/paises.service';
-import { DomiciliosService } from '../../../services/domicilios.service';
-import { Domicilio } from '../../../interfaces/domicilios';
+import { LocalidadesService } from '../../../services/localidades.service';
 
 
 @Component({
@@ -28,9 +26,11 @@ export class DomicilioComponent implements OnInit {
   add: boolean;
   errors: string;
 
-  selectedOption: any;
-  selectedOption2: any;
-  selectedLoc: any;
+  selectedPais: any;
+  selectedPais2: any;
+  selectedProvincia: any;
+  selectedLocalidad: any;
+  
 
 
   areaSelected: any;
@@ -44,45 +44,41 @@ export class DomicilioComponent implements OnInit {
 
   provincias: any;
   provinciasFiltradas = [];
+  localidadesFiltradas= [];
   paises: any;
-  //---
   localidades: any;
-  localiddesFiltradas = [];
-
 
   test: any;
 
   constructor(
     private fb: FormBuilder,
     private route: ActivatedRoute,
-    private localidadesServ: LocalidadesService,
-    private paisesService: PaisesService,
-    private router: Router,
-    //asdfg
     private domiciliosServ: DomiciliosService,
+    private paisesService: PaisesService,
+    private localidadesService: LocalidadesService,
+    private router: Router
   ) {}
 
   ngOnInit() {
-    //this.getPaises();
+
+    this.formGroupFormat();
+    this.getPaises();
     this.getProvincias();
     this.getLocalidades();
     this.idRute = this.route.snapshot.params.id;
     if (this.idRute) {
       this.getOne(this.idRute);
-      this.formGroupFormatID();
-    } else {
-      this.formGroupFormat();
     }
   }
 
 // SUBMIT METHODS----------------------------------------------------------
   onSubmit() {
     // agregar metodo que le pega a la api POST
-    this.postData(this.formGroup.value.localidades);
+    this.postData(this.formGroup.value.domicilios);
   }
   onSubmitId() {
     // agregar metodo que le pega a la api PUT
-    this.putData(this.formGroup.value.localidades);
+    this.putData(this.formGroup.value.domicilios);
   }
 // SUBMIT METHODS----------------------------------------------------------
 
@@ -90,20 +86,11 @@ export class DomicilioComponent implements OnInit {
 // CRUD METHODS------------------------------------------------------------
 
   getProvincias() {
-    this.localidadesServ.getProvincias().subscribe(
+    this.domiciliosServ.getProvincias().subscribe(
       res => {
         this.provincias = res;
+        //console.log(res);
         this.filterObsProv();  // ejecuta obs al devolver res de provincias
-      },
-      err => this.errors = err.error.text
-    );
-  }
-  //asdfg//
-  getLocalidades() {
-    this.localidadesServ.getLocalidades().subscribe(
-      res => {
-        this.localidades = res;
-        this.filterObsLoc();  // ejecuta obs al devolver res de provincias
       },
       err => this.errors = err.error.text
     );
@@ -113,10 +100,22 @@ export class DomicilioComponent implements OnInit {
     this.paisesService.getPaises().subscribe(
       res => {
         this.paises = res;
-        // console.log(res);
+        //console.log(res);
       }
     );
   }
+
+  getLocalidades() {
+    this.localidadesService.getLocalidades().subscribe(
+      res => {
+        this.localidades = res;
+        this.filterObsLocalidad();  // ejecuta obs al devolver res de provincias
+      
+       // console.log(res);
+      }
+    );
+  }
+
 
   getOne(id) {
     this.domiciliosServ.getDomicilio(id).subscribe(
@@ -126,45 +125,34 @@ export class DomicilioComponent implements OnInit {
 
         this.formGroup = this.fb.group({
           domicilios: this.fb.group({
-          id_dom: [{value: this.data.id_dom, disabled: true}],
+          id_domicilio: [{value: this.data.id_domicilio, disabled: true}],
           creado_en: [{value: this.datePipe.transform(this.data.creado_en, 'dd MMMM yyyy, HH:mm', '-600'), disabled: true}],
           calle: this.data.calle,
-          numero: this.data.numero,
-          piso: this.data.piso,
-          dpto: this.data.dpto,
-          id_localidad: this.data.id_localidad,
-          cod_postal: this.data.cod_postal,
-          telefono: this.data.telefono,
-          imagen: this.data.imagen,
-          latitud: this.data.latitud,
-          longitud: this.data.longitud,
-          localidades: [{value: this.data.localidad, disabled: true}],
+          nombre: this.data.nombre,
+          //cod_postal: this.data.cod_postal,
           provincias: [{value: this.data.provincia, disabled: true}],
           paises: [{value: this.data.pais, disabled: true}],
         }),
       }, { updateOn: 'change' });  // updateOn cambia la frecuencia en que se validan los inputs
-        // console.log(this.formGroup.value.localidades);
+        // console.log(this.formGroup.value.domicilios);
     },
       err => this.errors = err.error.text
     );
   }
 
   putData(body) {
-    // console.log('como viene del form: ', body);
-    // console.log('data', this.data);
-    this.domicilio.id_dom = this.data.id_dom;
-    this.domicilio.calle = body.calle;
-    this.domicilio.numero = body.numero;
-    this.domicilio.piso = body.piso;
-    this.domicilio.dpto = body.dpto;
-    this.domicilio.cod_postal = body.cod_postal;
-    this.domicilio.telefono = body.telefono;
-    this.domicilio.imagen = body.imagen;
-    this.domicilio.latitud = body.latitud;
-    this.domicilio.longitud = body.longitud;
-
-    this.domicilio.creado_en = new Date();
-    // console.log('antes de mandarlo', this.localidad);
+    console.log('como viene del form: ', body);
+    console.log('data', this.data);
+    // this.domicilio.id_dom = this.data.id_domicilio;
+    // this.domicilio.calle = body.calle;
+    // this.domicilio.numero= body.numero;
+    // this.domicilio.piso= body.piso;
+    // this.domicilio.dpto= body.dpto;
+    // this.domicilio.cod_postal = body.cod_postal;
+    // this.domicilio.telefono= body.telefono;
+    
+    //this.domicilio.creado_en = new Date();
+    console.log('antes de mandarlo', this.domicilio);
 
     this.domiciliosServ.updateDomicilio(this.idRute, this.domicilio).subscribe(
       res => {
@@ -179,31 +167,30 @@ export class DomicilioComponent implements OnInit {
   }
 
   postData(body) {
-    // console.log('como viene del form: ', body);
+    console.log('como viene del form: ', body);
     this.domicilio.calle = body.calle;
     this.domicilio.numero = body.numero;
     this.domicilio.piso = body.piso;
     this.domicilio.dpto = body.dpto;
     this.domicilio.cod_postal = body.cod_postal;
     this.domicilio.telefono = body.telefono;
-    this.domicilio.imagen = body.imagen;
-    this.domicilio.latitud = body.latitud;
-    this.domicilio.longitud = body.longitud;
-    // console.log('selected prov', this.selectedLoc);
-    this.domicilio.id_localidad = this.selectedLoc?this.selectedLoc.id_localidad:this.domicilio.id_localidad;
-    // console.log('antes de mandarlo: ', this.localidad);
+    // // console.log('selected prov', this.selectedProv);
+    // this.domicilio.id_provincia = this.selectedProv?this.selectedProv.id_provincia:this.domicilio.id_provincia;
+    //this.domicilio.id_localidad = this.selectedLocalidad?this.selectedLocalidad.id_localidad:this.domicilio.id_localidad;
+    
+    console.log('antes de mandarlo: ', this.domicilio);
 
-    this.domiciliosServ.saveDomicilio(this.domicilio).subscribe(
-        res => {
-          // this.errors = null;
-          this.add = true;
-          setTimeout(() => {
-            this.add = false;
-            this.router.navigate(['panel/domicilios']);
-          }, 1500);
-        },
-        err => this.errors = err.error.text
-    );
+    // this.domiciliosServ.saveDomicilio(this.domicilio).subscribe(
+    //     res => {
+    //       // this.errors = null;
+    //       this.add = true;
+    //       setTimeout(() => {
+    //         this.add = false;
+    //         this.router.navigate(['panel/domicilios']);
+    //       }, 1500);
+    //     },
+    //     err => this.errors = err.error.text
+    // );
   }
 
   delData() {
@@ -224,40 +211,17 @@ export class DomicilioComponent implements OnInit {
 
   formGroupFormat() {
     this.formGroup = this.fb.group({
-      localidades: this.fb.group({
+      domicilios: this.fb.group({
         id_dom: [{value: '', disabled: true}],
-        creado_en: [{value: '', disabled: true}],
         calle: ['', Validators.required],
         numero: ['', Validators.required],
-        piso: '',
-        dpto: '',
-        latitud: '',
-        longitud: '',
-        imagen: '',
-        cod_postal: '',
-        localidades: '',
-        provincias: '',
-        paises: ''
-      }),
-    }, { updateOn: 'change' });  // updateOn cambia la frecuencia en que se validan los inputs
-  }
-
-  formGroupFormatID() {
-    this.formGroup = this.fb.group({
-      localidades: this.fb.group({
-        id_localidad: [{value: '', disabled: true}],
-        creado_en: [{value: '', disabled: true}],
-        calle: ['', Validators.required],
-        numero: ['', Validators.required],
-        piso: [{value: '', disabled: true}],
-        dpto: [{value: '', disabled: true}],
-        latitud: [{value: '', disabled: true}],
-        longitud: [{value: '', disabled: true}],
-        imagen: [{value: '', disabled: true}],
-        cod_postal: [{value: '', disabled: true}],
-        localidades: [{value: '', disabled: true}],
-        provincias: [{value: '', disabled: true}],
-        paises: [{value: '', disabled: true}],
+        piso: ['', Validators.required],
+        dpto: ['', Validators.required],
+        cod_postal: ['', Validators.required],
+        telefono: ['', Validators.required],
+        provincias: ['', Validators.required],
+        paises: ['', Validators.required],
+        localidades: ['', Validators.required],
       }),
     }, { updateOn: 'change' });  // updateOn cambia la frecuencia en que se validan los inputs
   }
@@ -270,55 +234,53 @@ export class DomicilioComponent implements OnInit {
     .subscribe(res => {
       this.provinciasFiltradas.push(res);
     });
-    // console.log(this.provinciasFiltradas);
+     //console.log(this.provinciasFiltradas);
     if (this.provinciasFiltradas[0]) {
-      this.localidad.id_provincia = this.provinciasFiltradas[0].id_provincia;
+      //this.domicilio.id_localidad = this.provinciasFiltradas[0].id_provincia;
     }
-    // console.log(this.localidad);
+     //console.log(this.domicilio);
   }
 
-  filterObsProvSelected(item) {
-    const prueba = of (...this.provincias); // of hace obvservable al parametro y spread (...) lo desestructura
+  filterObsLocalidad(provincia?) {
+    this.localidadesFiltradas = [];
+    const prueba = of (...this.localidades); // of hace obvservable al parametro y spread (...) lo desestructura
+    prueba.pipe(
+    filter(res => res.provincia === (provincia?provincia:'Buenos Aires'))) // filtra buscando equivalencias
+    .subscribe(res => {
+      this.localidadesFiltradas.push(res);
+      //console.log(res);
+    });
+     //console.log(this.localidadesFiltradas);
+    if (this.provinciasFiltradas[0]) {
+      //this.domicilio.id_localidad = this.provinciasFiltradas[0].id_provincia;
+    }
+     //console.log(this.domicilio);
+  }
+
+  filterObsLocalidadSelected(item) {
+    const prueba = of (...this.localidades); // of hace obvservable al parametro y spread (...) lo desestructura
     prueba.pipe(
     filter(res => res.nombre === item )) // filtra buscando equivalencias
     .subscribe(res => {
-      this.selectedLoc = res;
-      // console.log(this.selectedLoc);
+      this.selectedLocalidad = res;
+      console.log(this.selectedLocalidad);
     });
   }
 
   getPaisSelected(item) {
-    this.selectedOption = item.target.value; // captura la opcion seleccionada
-    this.filterObsProv(this.selectedOption);
+    this.selectedPais = item.target.value; // captura la opcion seleccionada
+    this.filterObsProv(this.selectedPais);
+    //console.log(this.selectedPais);
   }
 
-  getProvinciaSelected(item) { // captura la opcion seleccionada
-    // console.log(this.selectedLoc);
-    this.filterObsProvSelected(item.target.value);
+  getProvinciaSelected(item) {
+    this.selectedProvincia = item.target.value; // captura la opcion seleccionada
+    this.filterObsLocalidad(this.selectedProvincia);
+    console.log(this.selectedProvincia);
+  }
+
+  getLocalidadSelected(item) { // captura la opcion seleccionada
+    // console.log(this.selectedProv);
+    this.filterObsLocalidadSelected(item.target.value);
   }
 }
-
-// filterObsLoc(provincia?){
-//   this.localidadesFiltradas = [];
-//   const prueba = of (...this.localidades); // of hace obvservable al parametro y spread (...) lo desestructura
-//   prueba.pipe(
-//   filter(res => res.provincia === (provincia?provincia:'Buenos Aires'))) // filtra buscando equivalencias
-//   .subscribe(res => {
-//     this.localidadesFiltradas.push(res);
-//   });
-//   // console.log(this.provinciasFiltradas);
-//   if (this.localidadesFiltradas[0]) {
-//     this.domicilio.id_localidad = this.localidadesFiltradas[0].id_localidad;
-//   }
-//   // console.log(this.localidad);
-// }
-
-// filterObsLocSelected(item) {
-//   const prueba = of (...this.localidades); // of hace obvservable al parametro y spread (...) lo desestructura
-//   prueba.pipe(
-//   filter(res => res.nombre === item )) // filtra buscando equivalencias
-//   .subscribe(res => {
-//     this.selectedLoc = res;
-//     // console.log(this.selectedLoc);
-//   });
-// }
